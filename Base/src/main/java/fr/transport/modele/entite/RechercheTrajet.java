2c636ebe849aceff9498e3cfeef5472500f9e801
Maxime Lemoine <maxime.lemoine1@etu.univ-lehavre.fr>
2024-04-12 01:03:32 +0200

    ajout des fichiers de structuration pour la recherche

---
+++ Base/src/main/java/fr/transport/modele/entite/RechercheTrajet.java
@@ -0,0 +1,149 @@
+package fr.transport.modele.entite;
+
+import java.util.List;
+import java.io.Serializable;
+import java.util.ArrayList;
+
+import fr.transport.modele.filtrage.FiltrageTrajets;
+import fr.transport.modele.filtrage.FiltreArretsOrdonnes;
+import fr.transport.modele.filtrage.FiltreTrajetJour;
+import fr.transport.modele.filtrage.FiltreTrajetPasseParArret;
+import fr.transport.modele.filtrage.FiltreTrajetPendantPeriode;
+
+public class RechercheTrajet implements Serializable
+{
+	private FiltrageTrajets filtre;
+
+	//TODO: ajouter aussi les heures
+	private List<Lieu> ensArretPassage;
+	private Lieu arretDepart;
+	private Lieu arretDestination;
+
+	private Periode periode;
+	private Jour jour;
+
+	/* CONSTRUCTEUR */
+
+	public RechercheTrajet( Lieu arretDepart, Lieu arretDestination )
+	{
+		this.filtre = new FiltrageTrajets();
+
+		this.ensArretPassage  = new ArrayList<Lieu>();
+		this.arretDepart      = arretDepart;
+		this.arretDestination = arretDestination;
+		
+		this.periode = null;
+		this.jour 	 = null;
+	}
+
+	/* ACCESSEURS */
+
+	public List<Lieu> getEnsArretPassage()
+	{
+		return this.ensArretPassage;
+	}
+
+	public Lieu getArretDepart()
+	{
+		return this.arretDepart;
+	}
+
+	public Lieu getArretDestination()
+	{
+		return this.arretDestination;
+	}
+
+	public Periode getPeriode()
+	{
+		return this.periode;
+	}
+
+	public Jour getJour()
+	{
+		return this.jour;
+	}
+
+	/* MODIFIEURS */
+
+	public void setArretDepart( Lieu nouvelArretDepart )
+	{
+		this.arretDepart = nouvelArretDepart;
+	}
+
+	public void setArretDestination( Lieu nouvelArretDestination )
+	{
+		this.arretDestination = nouvelArretDestination;
+	}
+
+	public void addArretPassage( Lieu arretPassage )
+	{
+		this.ensArretPassage.add( arretPassage );
+	}
+
+	public void setPeriode( Periode periode )
+	{
+		this.periode = periode;
+	}
+
+	public void setJour( Jour jour )
+	{
+		this.jour = jour;
+	}
+
+	/* METHODES */
+
+	public void reinitialiser()
+	{
+		this.ensArretPassage.clear();
+		this.periode = null;
+		this.jour = null;
+	}
+
+	public void filtrer( LigneTransport ligneTransport )
+	{
+		this.initialiserFiltre();
+		this.filtre.filtrer( ligneTransport );
+	}
+
+	private void initialiserFiltre()
+	{
+		this.filtre.resetFiltres();
+
+		this.ajouterFiltresArretPassage();
+		this.ajouterFiltresArretOrdonnes();
+		this.ajouterFiltresPeriode();
+		this.ajouterFiltresJour();
+	}
+
+	private void ajouterFiltresArretPassage( )
+	{
+		this.filtre.addFiltre( new FiltreTrajetPasseParArret( this.arretDepart ) );
+		this.filtre.addFiltre( new FiltreTrajetPasseParArret( this.arretDestination ) );
+
+		for( Lieu arretPassage : this.ensArretPassage )
+		{
+			this.filtre.addFiltre( new FiltreTrajetPasseParArret( arretPassage ) );
+		}
+	}
+
+	private void ajouterFiltresArretOrdonnes()
+	{
+		this.filtre.addFiltre( new FiltreArretsOrdonnes( this.arretDepart, this.arretDestination ) );
+	}
+
+	private void ajouterFiltresPeriode()
+	{
+		if( this.periode != null )
+		{
+			this.filtre.addFiltre( new FiltreTrajetPendantPeriode( this.periode ) );
+		}
+	}
+
+	private void ajouterFiltresJour()
+	{
+		if( this.jour != null )
+		{
+			this.filtre.addFiltre( new FiltreTrajetJour( this.jour ) );
+		}
+	}
+}
---
+++ Base/src/main/java/fr/transport/modele/outils/UtilitaireRechercheTrajet.java
@@ -0,0 +1,77 @@
+package fr.transport.modele.outils;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import fr.transport.modele.entite.*;
+
+public class UtilitaireRechercheTrajet
+{
+	private UtilitaireRechercheTrajet() {}
+
+	public static Map<Lieu, List<Heure>> getEnsembleHeuresParArrets( LigneTransport ligneTransport, RechercheTrajet rechercheTrajet )
+	{
+		Map<Lieu, List<Heure>> mapHeuresArrets = new LinkedHashMap<Lieu, List<Heure>>();
+
+		List<Lieu> ensArretAAfficher = new ArrayList<Lieu>();
+		ensArretAAfficher.add( rechercheTrajet.getArretDepart() );
+		ensArretAAfficher.addAll( rechercheTrajet.getEnsArretPassage() );
+		ensArretAAfficher.add( rechercheTrajet.getArretDestination() );
+
+		for( Lieu arret : ensArretAAfficher )
+		{
+			mapHeuresArrets.put( arret, new ArrayList<Heure>() );
+
+			for( Trajet trajet : ligneTransport.getEnsTrajet() )
+			{
+				Heure heure = trajet.getHeureArret( arret );
+				mapHeuresArrets.get( arret ).add( heure );
+			}
+		}
+
+		return mapHeuresArrets;
+	}
+
+	public static String heuresParArretsToString( Map<Lieu, List<Heure>> mapHeuresArrets )
+	{
+		StringBuilder sb = new StringBuilder();
+
+		for( Lieu arret : mapHeuresArrets.keySet() )
+		{
+			sb.append( String.format("%50s", arret) );
+			sb.append( " | " );
+
+			for( Heure heure : mapHeuresArrets.get( arret ) )
+			{
+				sb.append( String.format("%5s", heure) );
+				sb.append( " | " );
+			}
+
+			sb.append( "\n" );
+		}
+
+		return sb.toString();
+	}
+
+	public static String periodeEtJourToString( RechercheTrajet rechercheTrajet )
+	{
+		StringBuilder sb = new StringBuilder();
+
+		Periode periode = rechercheTrajet.getPeriode();
+		if( periode != null )
+		{
+			sb.append( periode );
+			sb.append( " - " );
+		}
+
+		Jour jour = rechercheTrajet.getJour();
+		if( jour != null )
+		{
+			sb.append( jour );
+		}
+
+		return sb.toString();
+	}
+}
