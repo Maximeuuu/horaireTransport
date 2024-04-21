eeabff28df892813f513c79613973eef21f8dfc8
Maxime Lemoine <maxime.lemoine1@etu.univ-lehavre.fr>
2024-04-22 00:58:25 +0200

    serialibilité

--- Base/src/main/java/fr/transport/modele/filtrage/IFiltre.java
+++ Base/src/main/java/fr/transport/modele/filtrage/IFiltre.java
@@ -2,7 +2,7 @@ package fr.transport.modele.filtrage;
 
 import fr.transport.modele.entite.LigneTransport;
 
-public interface IFiltre
+public interface IFiltre extends java.io.Serializable
 {
 	public void filtrer( LigneTransport ligneTransport );
 }
